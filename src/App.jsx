8d4e5ad7c44481ce23d78eb6b9a0c1616043d7f0
Augie De Blieck Jr <augiedb@gmail.com>
2023-08-17 07:27:32 -0400

    First card drawn shows on screen at load

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,64 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import PlayCard from './PlayCard.jsx'
+
 import './App.css'
+
+let deck = []
+
+const suits = ['hearts', 'diamonds', 'spades', 'clubs']
+const values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+
 
 function App() {
 
+  const [play_this_card, set_play_this_card] = useState({suit: 'Hello', val: '5'})
+ 
+
+function create_a_deck(suits, values) {
+  suits.map( (suit) => {
+    values.map( (val) => {
+      deck.push({ suit: suit, val: val})   
+    })
+  })
+  return deck
+} 
+
+function shuffleDeck(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array
+}
+ 
+
+  // When site is first loaded, start game immediately
+  // Later, we'll probably change this to a START button
+  useEffect(() => {
+    // Establish a deck of shuffled cards
+    let new_deck = create_a_deck(suits, values)
+    new_deck = shuffleDeck(new_deck)
+  
+    // Deal the first card?
+    set_play_this_card(play_this_card =>  new_deck.shift(new_deck))
+  
+    // Draw the game?
+
+  }, []);
+
+ 
   return (
-    <>
+   <>
+    <h1>Stack21 Game</h1> 
+          {/* <Stack deck={new_deck} />  */}
+          <PlayCard suit={play_this_card.suit} value={play_this_card.val} />
+
+          {/* <PassStack />
+
+          {[..Array(5).keys].map((row_num, i) => (
+            <Line row={row_num} />
+          ))}
+          <Score />  */}
 
     </>
   )
---
+++ src/PlayCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function PlayCard({suit, value}) {
+
+
+    return (
+        <div>
+
+            <h2>
+            {value} of {suit}
+            </h2>
+
+        </div>
+
+    )
+}
+
+
+export default PlayCard
